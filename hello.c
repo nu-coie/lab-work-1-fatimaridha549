127eac43f80ff81e3819530642dd62a42a26e816
fatimaridha549 <130349662+fatimaridha549@users.noreply.github.com>
2023-04-16 06:37:14 +0000

    ddd, Apr 4/16/2023, YYYY, 6:37 A +0

--- hello.c
+++ hello.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <cs50.h>
 int main(void){
+printf(hello world, answer./n);
 
 
 string name = get-string(what is your name?:");
-printf("hello %s\n" ,name);
+ 
 
 }
 {
